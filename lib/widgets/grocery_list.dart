f51e9e2261ab7920cdc90f2dcc502c3fd91334f1
M9heb <123528241+M9heb@users.noreply.github.com>
2025-06-10 08:25:18 +0430

    Adds the lottile animation

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:shopping_list/data/dummy_items.dart';
+import 'package:lottie/lottie.dart';
+import 'package:shopping_list/models/grocery_item.dart';
 import 'package:shopping_list/widgets/new_item.dart';
 
 class GroceryList extends StatefulWidget {
@@ -10,82 +11,73 @@ class GroceryList extends StatefulWidget {
 }
 
 class _GroceryListState extends State<GroceryList> {
-  void _addItem() {
-    Navigator.of(context).push(MaterialPageRoute(builder: (ctx) {
-      return NewItem();
-    }));
+  final List<GroceryItem> _groceryItems = [];
+  void _addItem() async {
+    final addedItem = await Navigator.of(context).push<GroceryItem>(
+      MaterialPageRoute(builder: (ctx) {
+        return NewItem();
+      }),
+    );
+    if (addedItem != null) {
+      setState(() {
+        _groceryItems.add(addedItem);
+      });
+    }
+  }
+
+  void _removeItem(groceryItem) {
+    setState(() {
+      _groceryItems.remove(groceryItem);
+    });
   }
 
   @override
   Widget build(BuildContext context) {
+    final Widget screenContents = _groceryItems.isEmpty
+        ? SizedBox(
+            width: double.infinity,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Lottie.asset("assets/empty_list.json"),
+                const SizedBox(height: 16),
+                const Text(
+                  "There's nothing here!",
+                  style: TextStyle(fontSize: 18),
+                ),
+              ],
+            ),
+          )
+        : ListView.builder(
+            itemCount: _groceryItems.length,
+            itemBuilder: (ctx, index) {
+              return Dismissible(
+                key: ValueKey(_groceryItems[index].id),
+                onDismissed: (direction) {
+                  _removeItem(_groceryItems[index]);
+                },
+                child: ListTile(
+                  leading: CircleAvatar(
+                    radius: 12,
+                    backgroundColor: _groceryItems[index].category.color,
+                  ),
+                  title: Text(
+                    _groceryItems[index].name,
+                    style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                        color: Theme.of(context).colorScheme.onSurface),
+                  ),
+                  trailing: Text("${_groceryItems[index].quantity}"),
+                ),
+              );
+            },
+          );
     return Scaffold(
       appBar: AppBar(
         title: const Text("Your Groceries"),
         actions: [IconButton(onPressed: _addItem, icon: Icon(Icons.add))],
       ),
-      body: ListView.builder(
-        itemCount: groceryItems.length,
-        itemBuilder: (ctx, index) {
-          return ListTile(
-            leading: CircleAvatar(
-              radius: 12,
-              backgroundColor: groceryItems[index].category.color,
-            ),
-            title: Text(
-              groceryItems[index].name,
-              style: Theme.of(context)
-                  .textTheme
-                  .bodyLarge!
-                  .copyWith(color: Theme.of(context).colorScheme.onSurface),
-            ),
-            trailing: Text("${groceryItems[index].quantity}"),
-          );
-        },
-        // children: [
-        //   ListTile(
-        //     leading: CircleAvatar(
-        //       radius: 16,
-        //       backgroundColor: Colors.cyan,
-        //     ),
-        //     title: Text(
-        //       "Milk",
-        //       style: Theme.of(context)
-        //           .textTheme
-        //           .bodyLarge!
-        //           .copyWith(color: Theme.of(context).colorScheme.surface),
-        //     ),
-        //     trailing: Text("1"),
-        //   ),
-        //   ListTile(
-        //     leading: CircleAvatar(
-        //       radius: 16,
-        //       backgroundColor: Colors.lightGreen,
-        //     ),
-        //     title: Text(
-        //       "Beef Steak",
-        //       style: Theme.of(context)
-        //           .textTheme
-        //           .bodyLarge!
-        //           .copyWith(color: Theme.of(context).colorScheme.surface),
-        //     ),
-        //     trailing: Text("1"),
-        //   ),
-        //   ListTile(
-        //     leading: CircleAvatar(
-        //       radius: 16,
-        //       backgroundColor: Colors.deepOrangeAccent,
-        //     ),
-        //     title: Text(
-        //       "Milk",
-        //       style: Theme.of(context)
-        //           .textTheme
-        //           .bodyLarge!
-        //           .copyWith(color: Theme.of(context).colorScheme.surface),
-        //     ),
-        //     trailing: Text("1"),
-        //   )
-        // ],
-      ),
+      body: screenContents,
     );
   }
 }
--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:shopping_list/data/categories.dart';
+import 'package:shopping_list/models/category.dart';
+import 'package:shopping_list/models/grocery_item.dart';
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -12,8 +14,21 @@ class NewItem extends StatefulWidget {
 
 class _NewItemState extends State<NewItem> {
   final _formKey = GlobalKey<FormState>();
+  var _enteredName = '';
+  var _enteredQuantity = 1;
+  var _selectedCategory = categories[Categories.vegetables]!;
+
   void _saveItem() {
-    _formKey.currentState!.validate();
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+      Navigator.of(context).pop(
+        GroceryItem(
+            id: DateTime.now().toString(),
+            name: _enteredName,
+            quantity: _enteredQuantity,
+            category: _selectedCategory),
+      );
+    }
   }
 
   @override
@@ -43,6 +58,9 @@ class _NewItemState extends State<NewItem> {
                       }
                       return null;
                     },
+                    onSaved: (value) {
+                      _enteredName = value!;
+                    },
                   ),
                   Row(
                     crossAxisAlignment: CrossAxisAlignment.end,
@@ -57,7 +75,7 @@ class _NewItemState extends State<NewItem> {
                           inputFormatters: [
                             FilteringTextInputFormatter.digitsOnly
                           ],
-                          initialValue: "1",
+                          initialValue: _enteredQuantity.toString(),
                           validator: (value) {
                             if (value == null ||
                                 value.isEmpty ||
@@ -67,30 +85,40 @@ class _NewItemState extends State<NewItem> {
                             }
                             return null;
                           },
+                          onSaved: (value) {
+                            _enteredQuantity = int.parse(value!);
+                          },
                         ),
                       ),
                       Expanded(
-                        child: DropdownButtonFormField(items: [
-                          for (final category in categories.entries)
-                            DropdownMenuItem(
-                                value: category.value,
-                                child: Row(spacing: 8, children: [
-                                  CircleAvatar(
-                                    radius: 8,
-                                    backgroundColor: category.value.color,
-                                  ),
-                                  Text(
-                                    category.value.title,
-                                    style: Theme.of(context)
-                                        .textTheme
-                                        .bodyLarge!
-                                        .copyWith(
-                                            color: Theme.of(context)
-                                                .colorScheme
-                                                .onSurface),
-                                  ),
-                                ]))
-                        ], onChanged: (value) {}),
+                        child: DropdownButtonFormField(
+                            value: _selectedCategory,
+                            items: [
+                              for (final category in categories.entries)
+                                DropdownMenuItem(
+                                    value: category.value,
+                                    child: Row(spacing: 8, children: [
+                                      CircleAvatar(
+                                        radius: 8,
+                                        backgroundColor: category.value.color,
+                                      ),
+                                      Text(
+                                        category.value.title,
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .bodyLarge!
+                                            .copyWith(
+                                                color: Theme.of(context)
+                                                    .colorScheme
+                                                    .onSurface),
+                                      ),
+                                    ]))
+                            ],
+                            onChanged: (value) {
+                              setState(() {
+                                _selectedCategory = value!;
+                              });
+                            }),
                       )
                     ],
                   ),
